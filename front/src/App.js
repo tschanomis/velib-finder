d5a5004ae550043a66d446dc581983904b16bed4
tschanomis <tschan.omis@gmail.com>
2020-01-12 20:07:10 +0100

    add geo

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Login from './components/Login'
+
+import Login from './components/Login';
+import AutoCompleteAdress from './components/AutoCompleteAdress';
 
 function App() {
   return (
     <div className="App">
       <Login/>
+      <AutoCompleteAdress/>
     </div>
   );
 }
