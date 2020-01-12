1fc274f286fd1f92bbc19194eba9a7c1c4244aaf
tschanomis <tschan.omis@gmail.com>
2020-01-12 15:04:45 +0100

    add jwt

--- back/routes/auth/auth.js
+++ back/routes/auth/auth.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require ('jsonwebtoken');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 
+const jwtSecret = require('../jwtSecret')
+
 router.get('/login', (req, res) => {
-	res.send("je suis dans auth")
+	res.send("je suis dans auth");
 })
 
 router.post('/login', (req, res) => {
-    console.log(req.body)
-    res.send(req.body)
+	if (req.body.email === "email@gmail.com" && req.body.password === "password") {
+		console.log("Login ok");
+		res.send(req.body);
+		const tokenUserInfo = {
+			email: req.body.email,
+			status: "status"
+		}
+		console.log(jwtSecret);
+		const token = jwt.sign(tokenUserInfo, jwtSecret);
+	}
 })
 
 module.exports = router;
