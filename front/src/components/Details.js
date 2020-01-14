dd399d8ef3469a0de740e577d2a17a73994943e4
tschanomis <tschan.omis@gmail.com>
2020-01-14 20:20:48 +0100

    adress detail done

--- front/src/components/Details.js
+++ front/src/components/Details.js
@@ -1,38 +1,48 @@
 import React from 'react';
+import axios from 'axios';
+
+import Adresses from './Adresses'
 
 import './Details.css'
 
 class Details extends React.Component {
 	state = {
-		items: [],
+		addresses: [],
+	}
+
+	handleclick = (e) => {
+		console.log(e.target.id)
 	}
 
 	render() {
 		return (
 			<div className="Details">
 				<div className="Card-list">
-					{this.props.items.slice(0, 5).map((item, i) => <div key={i} className="Card-station">
+					{this.props.items.slice(0, 5).map((item, i) => <div key={i} id={i} className="Card-station" onClick={this.handleclick}>
 						<div className="infos">
 							<div>
 								<h4>Nom de la station</h4>
 								<p>{item.Nom_de_la_station}</p>
 							</div>
-							<div>
-								<h4>Adresse</h4>
-								<p>wip</p>
-							</div>
-							<div>
-								<h4>Nombre de vélos</h4>
-								<p>{item.Nombre_vélo_électrique + item.Nombre_de_vélo_mécanique}</p>
-								<p>dont {item.Nombre_vélo_électrique} électique(s)</p>
+							<div className="hide">
+								<div>
+									<h4>Adresse</h4>
+									<Adresses lat={parseFloat(item.geo.split(',')[0])} lon={parseFloat(item.geo.split(',')[1])} />
+								</div>
+								<div>
+									<h4>Nombre de vélos</h4>
+									<p>{item.Nombre_vélo_électrique + item.Nombre_de_vélo_mécanique}</p>
+									<p>dont {item.Nombre_vélo_électrique} électrique(s)</p>
+								</div>
 							</div>
 						</div>
 						<div className="distance">
 							<h4>Distance : </h4>
-							<h5>{item.distance} Metres</h5>
+							<h5>{Math.round(item.distance)} Mètres</h5>
 						</div>
 					</div>)}
 				</div>
+				<button>button</button>
 			</div>
 		)
 	}
